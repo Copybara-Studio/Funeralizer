1ab6f0ff0f84c4f197a48144c88f999566ae467e
zbysiukaczucha <zs300533@student.polsl.pl>
2023-11-15 00:27:06 +0100

    Basic functionality added (loading image, converting to int arrat)

--- Funeralizer/Funeralizer/MainWindow.xaml.cs
+++ Funeralizer/Funeralizer/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Drawing;
+
 
 namespace Funeralizer
 {
@@ -16,9 +11,46 @@ namespace Funeralizer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string filePath;
         public MainWindow()
         {
             InitializeComponent();
+            filePath = "";
+        }
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                filePath = op.FileName;
+                imgPhoto.Source = new BitmapImage(new Uri(filePath));
+            }
+        }
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public int[] BitmapIntArray(string filePath)
+        {
+            Bitmap bmp = new Bitmap(filePath);
+            int[] rgb = new int[bmp.Width * bmp.Height * 3];
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color pixelColor = bmp.GetPixel(i, j);
+                    rgb[i * bmp.Height + j] = pixelColor.R;
+                    rgb[i * bmp.Height + j + 1] = pixelColor.G;
+                    rgb[i * bmp.Height + j + 2] = pixelColor.B;
+                }
+            }
+            return rgb;
         }
     }
+
 }
+
