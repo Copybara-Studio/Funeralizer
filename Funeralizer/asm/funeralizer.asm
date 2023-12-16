f6761edfea58b1c9f39a8e7463df2ca1e85db499
Zbigniew Serafin <zbysiu1501@gmail.com>
2023-12-16 17:36:39 +0100

    solved some issues

--- Funeralizer/asm/funeralizer.asm
+++ Funeralizer/asm/funeralizer.asm
@@ -1,52 +1,53 @@
 ;INCLUDE C:\masm32\include\windows.inc
 .CODE
 
-funeralize_asm PROC contents: DWORD, len: DWORD
+funeralize_asm PROC contents: QWORD, len: QWORD
 
 	; Clear the index register.
-	xor esi, esi
-
+	xor rsi, rsi
+	mov rdi, contents
 	; Beginning of loop instruction block.
 @loop:
 	
 	; Deal the R component.
-	mov eax, contents[esi]
+	
+	mov eax, [rdi+rsi]
 	mov ecx, 299
 	mul ecx
-	mov edx, 1000
-	div edx
-	mov contents[esi], eax
+	mov ebx, 1000
+	div ebx
+	mov [rdi+rsi], eax
 
 	; Prepare for the G component.
 	; Check whether it is valid!
-	inc esi
-	cmp esi, len
+	inc rsi
+	cmp rsi, len
 	jnc @error
 	
 	; Deal the G component.
-	mov eax, contents[esi]
+	mov eax, [rdi+rsi]
 	mov ecx, 587
 	mul ecx
-	mov edx, 1000
-	div edx
-	mov contents[esi], eax
+	mov ebx, 1000
+	div ebx
+	mov [rdi+rsi], eax
 	
 	; Prepare for the B component.
 	; Check whether it is valid!
-	inc esi
-	cmp esi, len
+	inc rsi
+	cmp rsi, len
 	jnc @error
 	
 	; Deal the B component.
-	mov eax, contents[esi]
+	mov eax, [rdi+rsi]
 	mov ecx, 114
 	mul ecx
-	mov edx, 1000
-	div edx
-	mov contents[esi], eax
+	mov ebx, 1000
+	div ebx
+	mov [rdi+rsi], eax
 
-	inc esi
-	cmp esi, len
+	inc rsi
+	cmp rsi, len
 	jc @loop
 	
 	mov eax, 1; Success
