40b51b90f67416799ef93f6958010d79aa8068da
Zbigniew Serafin <zbysiu1501@gmail.com>
2023-12-18 13:56:26 +0100

    deleted checking for error in asm code, more efficient and reliable division

--- Funeralizer/asm/funeralizer.asm
+++ Funeralizer/asm/funeralizer.asm
@@ -10,48 +10,31 @@ funeralize_asm PROC contents: QWORD, len: QWORD
 @loop:
 	
 	; Deal the R component.
-	
 	mov eax, [rdi+rsi]
 	mov ecx, 299
 	mul ecx
-	mov ebx, 1000
-	div ebx
 	mov [rdi+rsi], eax
-
-	; Prepare for the G component.
-	; Check whether it is valid!
-	mov rax, rsi
-	add rax, 1
-	cmp rax, len
-	jnc @error
 	
 	; Deal the G component.
 	mov eax, [rdi+rsi+1]
 	mov ecx, 587
 	mul ecx
-	mov ebx, 1000
-	div ebx
 	mov [rdi+rsi], eax
-	
-	; Prepare for the B component.
-	; Check whether it is valid!
-	mov rax, rsi
-	add rax, 2
-	cmp rax, len
-	jnc @error
 	
 	; Deal the B component.
 	mov eax, [rdi+rsi+2]
 	mov ecx, 114
 	mul ecx
-	mov ebx, 1000
-	div ebx
 	mov [rdi+rsi], eax
 
 	; Add the components together.
 	mov eax, [rdi+rsi]
 	add eax, [rdi+rsi+1]
 	add eax, [rdi+rsi+2]
+
+	; Divide by 1000.
+	mov ecx, 1000
+	div ecx
 
 	; Replace the components with the brightness.
 	mov [rdi+rsi], eax
@@ -64,9 +47,6 @@ funeralize_asm PROC contents: QWORD, len: QWORD
 	jc @loop
 	
 	mov eax, 1; Success
-	ret
-@error:
-	mov eax, 0; Failure
 	ret
 funeralize_asm ENDP
 
